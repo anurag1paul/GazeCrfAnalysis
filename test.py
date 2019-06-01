dffa9eb66cd64823a076a257ba2a43f367ae33b9
Anurag Paul <anurag1paul@gmail.com>
2019-06-01 02:36:46 -0700

    model and test code

--- test.py
+++ test.py
@@ -1,13 +1,12 @@
-import pycrfsuite
 import os
 import pickle
+import pycrfsuite
 
 from crfsuite_data import get_crf_features
 from reporting import looks_classification_report
 
 with open(os.path.join("data/out", "data.pkl"), "rb") as f:
     final_dicts = pickle.load(f)
-
 
 features_list = []
 ylabel_list = []
@@ -17,15 +16,16 @@ for fdict in final_dicts:
     features_list.append(features)
     ylabel_list.append(ylabel)
 
-trainer = pycrfsuite.Trainer(verbose=False)
+tagger = pycrfsuite.Tagger()
+tagger.open('exp1')
+
+y_pred = []
+y_true = []
 
 for features, ylabel in zip(features_list[:-1], ylabel_list[:-1]):
     for i in range(0, len(features), 5):
-        trainer.append(features[i:i+20], ylabel[i:i+20])
+        y_pred.append(tagger.tag(features[i:i+20]))
+        y_true.append(ylabel[i:i+20])
 
-trainer.set_params({'c1': 0.1,   # coefficient for L1 penalty
-                    'c2': 0.1,  # coefficient for L2 penalty
-                    'max_iterations': 100000,  # stop earlier
-                    'feature.possible_transitions': True})
+print(looks_classification_report(y_true, y_pred))
 
-trainer.train("exp1")
