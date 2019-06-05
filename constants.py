2ab99f97f22c8935fc23c38285a878b83dace288
Anurag Paul <anurag1paul@gmail.com>
2019-06-05 13:43:13 -0700

    fixing validation issue

---
+++ constants.py
@@ -0,0 +1,5 @@
+# constants to be used for the project
+
+FILE_CHUNK = 500
+NUMBER_OF_CV_FOLDS = 5
+
--- data_processing.py
+++ data_processing.py
@@ -7,11 +7,13 @@ import pickle
 
 from collections import defaultdict
 
+from constants import FILE_CHUNK
 
 root_dir = "data/"
 object_det = os.path.join(root_dir, "object_detections")
 gaze = os.path.join(root_dir, "gaze")
 gt = os.path.join(root_dir, "annotations")
+
 
 def bbox_normalized_coords(bbox):
     x = bbox[0] / 1280
@@ -20,14 +22,14 @@ def bbox_normalized_coords(bbox):
     h = bbox[3] / 720
     return x, y, w, h
 
-def read_object_detections(filename):
 
+def read_object_detections(filename):
     with open(os.path.join(object_det, filename)) as f:
         object_data = f.readlines()
 
     frame_list = []
     bbox_list = []
-    
+
     for line in object_data:
         frame = defaultdict(list)
         bbox = defaultdict(list)
@@ -42,7 +44,7 @@ def read_object_detections(filename):
                     if obj_name == "cards":
                         obj_name = "card"
                     frame[obj_name].append((x, y, obj[1]))
-                    bbox[obj_name + "_bbox"].append((x-w/2, y-h/2, x+w/2, y+h/2))
+                    bbox[obj_name + "_bbox"].append((x - w / 2, y - h / 2, x + w / 2, y + h / 2))
             else:
                 obj = ast.literal_eval(a)
                 x, y, w, h = bbox_normalized_coords(obj[2])
@@ -50,11 +52,11 @@ def read_object_detections(filename):
                 if obj_name == "cards":
                     obj_name = "card"
                 frame[obj_name].append((x, y, obj[1]))
-                bbox[obj_name + "_bbox"].append((x-w/2, y-h/2, x+w/2, y+h/2))
+                bbox[obj_name + "_bbox"].append((x - w / 2, y - h / 2, x + w / 2, y + h / 2))
 
         frame_list.append(frame)
         bbox_list.append(bbox)
-        
+
     return frame_list, bbox_list
 
 
@@ -78,49 +80,49 @@ def read_gaze_file(filename):
 
 def get_frame_gaze_dict(gaze_list, frame_list, bbox_list, max_idx):
     out = {
-          "card":[],
-          "face":[],
-          "dice":[],
-          "key":[],
-          "map":[],
-          "ball":[]
-          }
-    bbox_out = {"card_bbox":[],
-          "face_bbox":[],
-          "dice_bbox":[],
-          "key_bbox":[],
-          "map_bbox":[],
-          "ball_bbox":[]}
+        "card": [],
+        "face": [],
+        "dice": [],
+        "key": [],
+        "map": [],
+        "ball": []
+    }
+    bbox_out = {"card_bbox": [],
+                "face_bbox": [],
+                "dice_bbox": [],
+                "key_bbox": [],
+                "map_bbox": [],
+                "ball_bbox": []}
 
     for i in range(max_idx):
-        
+
         pupil = gaze_list[i]
         frame = frame_list[i]
         bbox_l = bbox_list[i]
-        
+
         for key in out.keys():
-            
+
             if key in frame:
                 dists = []
-                
+
                 for pt in frame[key]:
-                    dist = ((pupil[0] - pt[0])**2 + (pupil[1] - pt[1])**2)**0.5
+                    dist = ((pupil[0] - pt[0]) ** 2 + (pupil[1] - pt[1]) ** 2) ** 0.5
                     if math.isnan(dist):
                         print(pupil, pt)
                     else:
                         dists.append(dist)
-                
+
                 min_dist = min(dists)
-                out[key].append((2-min_dist)/2)
+                out[key].append(min_dist)
             else:
-                out[key].append(0)
-                
+                out[key].append(1)
+
         for key in bbox_out.keys():
             is_in = False
             if key in bbox_l:
                 for bbox in bbox_l[key]:
                     if ((bbox[0] <= pupil[0] <= bbox[2]) and
-                        (bbox[1] <= pupil[1] <= bbox[3])):
+                            (bbox[1] <= pupil[1] <= bbox[3])):
                         is_in = True
                         break
             bbox_out[key].append(is_in)
@@ -130,16 +132,15 @@ def get_frame_gaze_dict(gaze_list, frame_list, bbox_list, max_idx):
 
 
 def read_looks_gt_file(filename, out, max_idx, tpf):
-
     looks = pd.read_csv(os.path.join(gt, filename))[["object", "start_sec", "end_sec"]]
     looks = looks.sort_values("start_sec")
-    out["look"] = np.array([0]*max_idx)
-    labels = {"card":1,
-          "face":2,
-          "dice":3,
-          "key":4,
-          "map":5,
-          "ball":6}
+    out["look"] = np.array([0] * max_idx)
+    labels = {"card": 1,
+              "face": 2,
+              "dice": 3,
+              "key": 4,
+              "map": 5,
+              "ball": 6}
     for row in looks.values:
         start = int(np.floor(row[1] / tpf))
         end = int(np.ceil(row[2] / tpf))
@@ -149,8 +150,17 @@ def read_looks_gt_file(filename, out, max_idx, tpf):
     return out
 
 
-def main():
+def create_chunks(final_dicts, chunk_size):
+    chunks = []
+    for data in final_dicts:
+        num_frames = len(data)
+        num_chunks = num_frames // chunk_size
+        for i in range(num_chunks):
+            chunks.append(data[i*chunk_size:(i+1)*chunk_size])
+    return chunks
 
+
+def main():
     obj_det_files = ["Andy.csv", "Daniel.csv", "2018_07_17_001.csv", "2018_07_24_003.csv", "2018_07_17_004.csv"]
     gaze_files = ["Andy_gaze_positions.csv", "Daniel_gaze_positions.csv",
                   "2018_07_17_001_gaze_positions.csv", "2018_07_24_003_gaze_positions.csv",
@@ -188,8 +198,11 @@ def main():
     if not os.path.exists(out_dir):
         os.makedirs(out_dir)
 
+    chunk_dicts = create_chunks(final_dicts, FILE_CHUNK)
+
     with open(os.path.join(out_dir, "data.pkl"), "wb") as f:
-        pickle.dump(final_dicts, f)
+        pickle.dump(chunk_dicts, f)
+
 
 if __name__ == "__main__":
     main()
